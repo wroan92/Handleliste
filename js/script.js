62e36738b79eb85e0c54b866a2e9e3d2798b502a
wroan92 <Anders-wroldsen@live.com>
2023-01-30 12:09:36 +0100

    30.01

--- js/script.js
+++ js/script.js
@@ -5,31 +5,43 @@ const removeBtn = document.querySelector("#removeBtn");
 
 const products = ["Melk", "Saft", "Egg"];
 
-function updateProductList() {
-  productList.innerHTML = products.map(product => `<li>${product}</li>`).join('');
+// Function to update productList
+updateProductList = () => {
+  productList.innerHTML = products.map((product, index) => `<li onclick="removeProduct(${index})">${product}</li>`).join('');
+  productInput.value = "";
 }
 
 productSubmitBtn.addEventListener("click", () => {
   products.push(productInput.value);
-  productInput.value = "";
+  
   updateProductList();
 });
 
 productInput.addEventListener("keypress", event => {
     if (event.key === "Enter") {
         products.push(productInput.value);
-        productInput.value = "";
+        
         updateProductList();
     }
 });
 
 removeBtn.addEventListener("click", () => {
-  if(products.length > 0){
+  if(products.length > 1){
     products.pop();
     updateProductList();
   }else{
     productList.innerHTML = "Ingen produkter og fjerne.";
   }
 });
+
+removeProduct = (i) =>{
+  if(products.length >1){
+    products.splice(i, 1)
+    updateProductList();
+  }else{
+    productList.innerHTML = "Ingen produkter og fjerne.";
+  }
+ 
+}
 
 updateProductList();
