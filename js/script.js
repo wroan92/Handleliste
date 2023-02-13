b0297b4b6f93ba3eb7079897d6d32da47b1763ad
wroan92 <Anders-wroldsen@live.com>
2023-02-13 20:46:32 +0100

    Added qty +1 line 26-46

--- js/script.js
+++ js/script.js
@@ -3,27 +3,47 @@ const productInput = document.querySelector("#productInput");
 const productSubmitBtn = document.querySelector("#productSubmit");
 const removeBtn = document.querySelector("#removeBtn");
 
-const products = ["Melk", "Saft", "Egg"];
+/* const products = ["Melk", "Saft", "Egg"]; */
+var products = [
+]
 
-// Function to update productList
+// Function to update productLList
+/* updateProductList = () => {
+  productList.innerHTML = products.map((x, index) => `<li onclick="removeProduct(${index})">${x}</li>`).join('');
+
+  productInput.value = "";
+} */
 updateProductList = () => {
-  productList.innerHTML = products.map((product, index) => `<li onclick="removeProduct(${index})">${product}</li>`).join('');
+  productList.innerHTML = products.map((x, index) => `<li onclick="removeProduct(${index})">${x.product} <br>Ant: ${x.qty}</li><hr>`).join('');
   productInput.value = "";
 }
 
-productSubmitBtn.addEventListener("click", () => {
+/* productSubmitBtn.addEventListener("click", () => {
   products.push(productInput.value);
   
   updateProductList();
-});
+}); */
+productSubmitBtn.addEventListener("click", () => {
+  const existingProduct = products.find(x => x.product.toLowerCase() === productInput.value.toLowerCase());
+  if (existingProduct) {
+  existingProduct.qty += 1;
+  } else {
+  products.push({ product: productInput.value, qty: 1 });
+  }
+  updateProductList();
+  });
 
-productInput.addEventListener("keypress", event => {
+  productInput.addEventListener("keypress", event => {
     if (event.key === "Enter") {
-        products.push(productInput.value);
-        
-        updateProductList();
+    const existingProduct = products.find(x => x.product.toLowerCase() === productInput.value.toLowerCase());
+    if (existingProduct) {
+    existingProduct.qty += 1;
+    } else {
+    products.push({ product: productInput.value, qty: 1 });
     }
-});
+    updateProductList();
+    }
+    });
 
 removeBtn.addEventListener("click", () => {
   if(products.length > 1){
